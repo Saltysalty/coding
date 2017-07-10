caaeb377f0f242055051a71379305a887642ca40
Julija <julija.kaupelyte@gmail.com>
2017-07-10 20:58:52 +0300

    FINAL

--- service/support/insert.php
+++ service/support/insert.php
@@ -1,25 +1,35 @@
 <?php
-	require_once('../db/connect.php');
+	// Apsiraðome parametrus
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "codingchicken";
 
+	// Sukuriamas prisijungimas
+	$conn = new mysqli($servername, $username, $password, $dbname);
 	
-	$stmt = $conn->prepare("INSERT INTO support (fname, email, message, messagedate) 
+	// Patikriname prisijungimà
+	if($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	} 
+	
+	$stmt = $conn->prepare("INSERT INTO messages (name, email, text, date) 
 										VALUES (?, ?, ?, now())");
 	
-	$stmt->bind_param("ss", $fname, $email, $message);
-	$fname = $_POST["fname"];
+	$stmt->bind_param("sss", $name, $email, $text); // "sss" nurodome elementu tipa kiekvienam elementui
+	$name = $_POST["fname"];
 	$email = $_POST["email"];
-	$message = $_POST["message"];
+	$text = $_POST["message"];
 	// set parameters and execute
 	if ($stmt->execute()) { 
-		echo "OK";
-		header('Location: /index.php');
+		header('Location: /contact.php');
 	} else {
 	   echo $stmt->error;
 	}
 	
 	$stmt->close();
 	
-	require_once('../db/disconnect.php');
+	$conn->close();
 	
 	// Nukreipiame á kità svetainæ
 	//header('Location: /LAYOUT/asmeninis/index.php');
